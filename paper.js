af3d14a47aacdc09c7738868dd8f5fed7916efe8
SakshamNaik123 <78081850+SakshamNaik123@users.noreply.github.com>
2021-02-18 10:26:43 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class paper
+{ 
+  constructor(x,y,r) 
+  { 
+    var options={ 
+      isStatic:false,
+        restitution:0.3, 
+      friction:0.5, 
+      density:1.2 
+  } 
+  this.image=loadImage("paper.png");
+  
+  this.x=x; 
+  this.y=y; 
+  this.r=r 
+  this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+   World.add(world, this.body); 
+  } 
+  display() 
+  { 
+    var rubberpos=this.body.position; 
+    push() 
+    translate(rubberpos.x, rubberpos.y); 
+    rectMode(CENTER) 
+    
+    ellipse(0,0,this.r, this.r); 
+    pop() 
+  } 
+}
